fa03fecd614ec727a16956d1c593e807c840159d
MARS <wangyunfan@vtstar.net>
2019-05-01 22:26:44 +0800

    fea: 添加Lesson-1

---
+++ Lesson-1/greeter.ts
@@ -0,0 +1,23 @@
+class Student {
+    fullName: string;
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+console.table(user)
+
+// document.body.innerHTML = JSON.stringify(greeter({
+// 	firstName: "Tom",
+// 	lastName: "kate"
+// }));
