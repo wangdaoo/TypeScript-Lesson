291b1f26db6699083aba3aebe99eabe715f0fed8
MARS <wangyunfan@vtstar.net>
2019-05-11 20:56:45 +0800

    fea: 可省略的参数的使用

---
+++ src/Lesson-12.ts
@@ -0,0 +1,20 @@
+// !可省略的参数
+
+// ?通过给 name 后面加上 ? , 达到可给函数传空的效果
+// function sayHello(name?: string): string {
+// 	if (name === undefined) {
+// 		return "Hello Mars!"
+// 	} else {
+// 		return "Hello " + name + "!"
+// 	}
+// }
+
+// ?通过函数加上默认值,达到传空的目的
+function sayHello(name: string = 'Mars'): string {
+	return "Hello " + name + "!"
+}
+console.warn(sayHello("Trump"))
+// Hello Trump
+
+console.error(sayHello())
+// Hello Mars
